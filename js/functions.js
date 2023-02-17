afaeffefdc6d37c8116f2ff9d25527462eb67da9
Ricardo Leal <richardrlp91@gmail.com>
2023-02-17 15:13:28 +0100

    Este es el inicio del directorio personal definitivo

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+let theme=document.getElementById("theme");
+var color_theme=document.getElementById("color_theme");
+alert(typeof color_theme);
+alert(color_theme);
+// color_theme.addEventListener("change",()=>{
+//     switch(color_theme.value){
+//         case "light":
+//             alert("Es light");
+//             theme.removeAttribute[href];
+//             theme.setAttribute[href="dark"];
+//             break;
+//         case color_theme.value=='dark':
+//             alert("es dark");
+//             theme.removeAttribute[href];
+//             theme.setAttribute[href="light"];
+//             break;
+//         }
+// });
