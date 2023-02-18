99cd91f6bab451e28fac34939f9d63b081671d4d
Richardrlp91 <richardrlp91@gmail.com>
2023-02-18 18:26:26 +0100

    Se ha creado la funcion de cambio de tema y se esta trabajando en el menú

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,25 @@
 let theme=document.getElementById("theme");
-var color_theme=document.getElementById("color_theme");
-alert(typeof color_theme);
-alert(color_theme);
-// color_theme.addEventListener("change",()=>{
-//     switch(color_theme.value){
-//         case "light":
-//             alert("Es light");
-//             theme.removeAttribute[href];
-//             theme.setAttribute[href="dark"];
-//             break;
-//         case color_theme.value=='dark':
-//             alert("es dark");
-//             theme.removeAttribute[href];
-//             theme.setAttribute[href="light"];
-//             break;
-//         }
-// });
+var select_theme=document.getElementById("select_theme");
+select_theme.addEventListener('change',function(){
+    var option__select_theme=this.options[select_theme.selectedIndex];
+    if(option__select_theme.value=='dark'){
+        theme.setAttribute('href','styles/theme-dark.css');
+    }else{
+        theme.setAttribute('href','styles/theme-light.css');
+
+    };
+
+//     console.log(pro);
+//     // switch(color_theme.value){
+//     //     case "light":
+//     //         alert("Es light");
+//     //         theme.removeAttribute[href];
+//     //         theme.setAttribute[href="dark"];
+//     //         break;
+//     //     case color_theme.value=='dark':
+//     //         alert("es dark");
+//     //         theme.removeAttribute[href];
+//     //         theme.setAttribute[href="light"];
+//     //         break;
+//     //     }
+});
